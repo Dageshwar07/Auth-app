3d372e319f0b6011d02f63c4b7b9fcc54ecb6d0d
Dageshwar Das <dageshwardasmanikpuri996@gmail.com>
2023-10-05 14:48:44 +0530

    update

--- Controllers/Auth.js
+++ Controllers/Auth.js
@@ -6,123 +6,123 @@ require("dotenv").config();
 
 
 //signup route handler
-exports.signup = async (req,res) => {
-    try{
+exports.signup = async (req, res) => {
+    try {
         //get data
-        const {name, email, password, role} = req.body;
+        const { name, email, password, role } = req.body;
         //check if user already exist
-        const existingUser = await User.findOne({email});
+        const existingUser = await User.findOne({ email });
 
-        if(existingUser){
+        if (existingUser) {
             return res.status(400).json({
-                success:false,
-                message:'User already Exists',
+                success: false,
+                message: 'User already Exists',
             });
         }
 
         //secure password
         let hashedPassword;
-        try{
+        try {
             hashedPassword = await bcrypt.hash(password, 10);
         }
-        catch(err) {
+        catch (err) {
             return res.status(500).json({
-                success:false,
-                message:'Error inn hashing Password',
+                success: false,
+                message: 'Error inn hashing Password',
             });
         }
 
         //create entry for User
         const user = await User.create({
-            name,email,password:hashedPassword,role
+            name, email, password: hashedPassword, role
         })
 
         return res.status(200).json({
-            success:true,
-            message:'User Created Successfully',
+            success: true,
+            message: 'User Created Successfully',
         });
 
     }
-    catch(error) {
+    catch (error) {
         console.error(error);
         return res.status(500).json({
-            success:false,
-            message:'User cannot be registered, please try again later',
+            success: false,
+            message: 'User cannot be registered, please try again later',
         });
     }
 }
 
 
 //login
-exports.login = async (req,res) => {
+exports.login = async (req, res) => {
     try {
 
         //data fetch
-        const {email, password} = req.body;
+        const { email, password } = req.body;
         //validation on email and password
-        if(!email || !password) {
+        if (!email || !password) {
             return res.status(400).json({
-                success:false,
-                message:'PLease fill all the details carefully',
+                success: false,
+                message: 'PLease fill all the details carefully',
             });
         }
 
         //check for registered user
-        let user = await User.findOne({email});
+        let user = await User.findOne({ email });
         //if not a registered user
-        if(!user) {
+        if (!user) {
             return res.status(401).json({
-                success:false,
-                message:'User is not registered',
+                success: false,
+                message: 'User is not registered',
             });
         }
 
         const payload = {
-            email:user.email,
-            id:user._id,
-            role:user.role,
+            email: user.email,
+            id: user._id,
+            role: user.role,
         };
         //verify password & generate a JWT token
-        if(await bcrypt.compare(password,user.password) ) {
+        if (await bcrypt.compare(password, user.password)) {
             //password match
-            let token =  jwt.sign(payload, 
-                                process.env.JWT_SECRET,
-                                {
-                                    expiresIn:"2h",
-                                });
+            let token = jwt.sign(payload,
+                process.env.JWT_SECRET,
+                {
+                    expiresIn: "2h",
+                });
 
-                                
+
 
             user = user.toObject();
             user.token = token;
             user.password = undefined;
 
             const options = {
-                expires: new Date( Date.now() + 3 * 24 * 60 * 60 * 1000),
-                httpOnly:true,
+                expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+                httpOnly: true,
             }
 
-            res.cookie("babbarCookie", token, options).status(200).json({
-                success:true,
+            res.cookie("dageshwar", token, options).status(200).json({
+                success: true,
                 token,
                 user,
-                message:'User Logged in successfully',
+                message: 'User Logged in successfully',
             });
         }
         else {
             //passwsord do not match
             return res.status(403).json({
-                success:false,
-                message:"Password Incorrect",
+                success: false,
+                message: "Password Incorrect",
             });
         }
 
     }
-    catch(error) {
+    catch (error) {
         console.log(error);
         return res.status(500).json({
-            success:false,
-            message:'Login Failure',
+            success: false,
+            message: 'Login Failure',
         });
 
     }
--- config/database.js
+++ config/database.js
@@ -5,13 +5,13 @@ require("dotenv").config();
 
 exports.connect = () => {
     mongoose.connect(process.env.MONGODB_URL, {
-        useNewUrlParser:true,
-        useUnifiedTopology:true
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     })
-    .then(() => {console.log("DB connected successfully")})
-    .catch( (err) => {
-        console.log("DB CONNECTION ISSUES");
-        console.error(err);
-        process.exit(1);
-    } );
+        .then(() => { console.log("DB connected successfully") })
+        .catch((err) => {
+            console.log("DB CONNECTION ISSUES");
+            console.error(err);
+            process.exit(1);
+        });
 }
